3278200e8a47918658006e31973e71376b2038b8
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-07-16 21:50:37 +0100

    first commit

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://mohamed:ZT2owmZeudtFhxdX@cluster0.m1iqdyq.mongodb.net/?retryWrites=true&w=majority", {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
+}
